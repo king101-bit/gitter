c6eb56a78bcef7174ba545d1e1631351b18e1a3e
Zack <zackagba7@gmail.com>
2026-02-16 18:40:18 +0100

    feat: Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+import { execSync } from 'child_process';
+import readline from 'readline';
+
+let diff = '';
+try {
+    diff = execSync('git diff --staged', { encoding: 'utf-8' });
+
+} catch {
+    console.log('Not a git repository.');
+    process.exit(1);
+}
+
+if (!diff.trim()) {
+    console.log('No staged changes found. Stage files first.'); 
+    process.exit(0);
+}
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+const ask = (q) =>
+    new Promise((res) => rl.question(q, (ans) => res(ans.trim())));
+
+
+(async () => {
+    const type = await ask("type (feat, fix, docs, style, refactor, perf, test, chore): ");
+    const desc = await ask("description: ");
+
+    rl.close();
+
+    const safeDesc = desc.replace(/"/g, '\\"');
+    const message = `${type}: ${safeDesc}`;
+
+    try {
+        execSync(`git commit -m "${message}"`, { stdio: 'inherit', });
+    } catch {
+        console.log('Commit failed.');
+    }
+})();
